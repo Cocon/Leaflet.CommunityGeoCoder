2f16c6616da2671181987597609914f2f0faa7a1
YUUKIToriyama <github@toriyama.dev>
2021-07-30 13:10:50 +0900

    update src/Interface.tsx

--- src/Interface.tsx
+++ src/Interface.tsx
@@ -18,10 +18,16 @@ const Interface: React.FunctionComponent<InterfaceProps> = (props) => {
 	const inputRef = React.useRef<HTMLInputElement>(null);
 	const buttonRef = React.useRef<HTMLButtonElement>(null);
 	const formStyle: React.CSSProperties = {
-		display: "flex"
+		display: "flex",
 	};
 	const buttonStyle: React.CSSProperties = {
-		width: "50px"
+		width: "50px",
+		height: "50px",
+		border: "none",
+		cursor: "pointer",
+		appearance: "none",
+		backgroundImage: `url("https://github.com/geolonia/logo/raw/master/svg/symbol.svg")`,
+		backgroundSize: "cover"
 	};
 
 	React.useEffect(() => {
@@ -33,7 +39,7 @@ const Interface: React.FunctionComponent<InterfaceProps> = (props) => {
 	return (
 		<div style={formStyle} className="custom-panel leaflet-bar">
 			<input type="search" placeholder="例: 東京都千代田区霞が関1-3-1" ref={inputRef} />
-			<button style={buttonStyle} ref={buttonRef}>Go!</button>
+			<button style={buttonStyle} ref={buttonRef} title="Powered by geolonia/normalize-japanese-addresses">Go!</button>
 		</div>
 	)
 }
