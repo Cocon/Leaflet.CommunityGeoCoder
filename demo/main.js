895f62b7552d59a5366081ceb3b498da041286b9
YUUKIToriyama <github@toriyama.dev>
2021-07-31 20:25:51 +0900

    update demo/ - ソースコードを表示するようにした

---
+++ demo/main.js
@@ -0,0 +1,9 @@
+const map = new L.map("my_map", {
+	center: [35.4477712, 139.6425415],
+	zoom: 13
+});
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+const geoCoderControl = new GeoCoder();
+geoCoderControl.addTo(map);
