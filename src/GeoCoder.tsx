cdce4c51e4adace92dd5afd34a23f12d2a08cf5e
YUUKIToriyama <github@toriyama.dev>
2021-08-12 20:38:08 +0900

    入力された住所が短い場合、おおまかな位置を取得し移動

--- src/GeoCoder.tsx
+++ src/GeoCoder.tsx
@@ -30,11 +30,31 @@ export class GeoCoder extends L.Control {
 		return async (address: string) => {
 			const result = await Geolonia.normalize(address);
 			console.log(result);
-			// level 3 まで判別できた場合には、緯度経度情報も返ってくる
-			// https://github.com/geolonia/normalize-japanese-addresses/pull/113
 			if (result.level >= 3) {
-				const destination = L.latLng(result.lat as number, result.lng as number);
-				Utils.moveTo(map, destination);
+				// level 3 まで判別できた場合には、緯度経度情報も返ってくる
+				// https://github.com/geolonia/normalize-japanese-addresses/pull/113
+				const latlng = L.latLng({
+					lat: result.lat || 35,
+					lng: result.lng || 135
+				});
+				Utils.moveTo(map, latlng);
+			} else if (result.level == 2) {
+				// level 2 までしか判別できなかった場合は、
+				// 別途データベースに問い合わせ、大まかな位置情報を取得する
+				const url = `https://geolonia.github.io/japanese-addresses/api/ja/${result.pref}/${result.city}.json`;
+				fetch(url).then(response => response.json()).then(json => {
+					const destination = json[0] as {
+						town: string
+						koaza: string
+						lat: string
+						lng: string
+					};
+					const latlng = L.latLng({
+						lat: parseFloat(destination.lat),
+						lng: parseFloat(destination.lng)
+					});
+					Utils.moveTo(map, latlng);
+				});
 			} else {
 				alert("もう少し詳しい住所を入力してください");
 			}
