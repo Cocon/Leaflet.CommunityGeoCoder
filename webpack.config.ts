01bf5e5b4ba8054eb35df5acb0f04ab15d376afb
YUUKIToriyama <github@toriyama.dev>
2021-08-12 22:10:35 +0900

    拡張子tsxを削除

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,7 +2,7 @@ import { Configuration } from 'webpack';
 
 const config: Configuration = {
 	entry: {
-		'bundle': ["./src/index.tsx"]
+		'bundle': ["./src/index.ts"]
 	},
 	externals: {
 		leaflet: "L"
@@ -18,7 +18,7 @@ const config: Configuration = {
 	module: {
 		rules: [
 			{
-				test: /\.tsx?$/,
+				test: /\.ts$/,
 				use: 'ts-loader',
 			},
 			{
